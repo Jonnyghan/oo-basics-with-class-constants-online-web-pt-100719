508ee29d8ac36cc4edb30fc9fcd124e5f85f1021
Jonathan Ghandforoush <jonnyghan@gmail.com>
2019-11-04 17:27:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,17 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+  
 
+  BRANDS = []
+  
+  def brand=(brand)
+    @brand=brand
+  end
+  
+  def brand
+    @brand
+  end
+  
   def initialize(brand)
     @brand = brand
   end
